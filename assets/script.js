25d4b2feff6bd48994f486a33281be220f494301
Kaminishikawahara KK <kkaori146@gmail.com>
2021-11-02 13:54:36 +0900

    CPU implementation: Level 1

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,53 @@ var nivel=1;
 var jogadaCpu=1;
 var quemComeca=1;
 
+function cpuJoga() {
+    if(jogando) {
+        var l, c;
+        if(nivel==1){
+            do {
+                l=Math.round(Math.random()*2);
+                c=Math.round(Math.random()*2);
+            }while(jogo[l][c]!="");
+            jogo[l][c]="0";
+        } else if(nivel==2){
+
+        }
+        verifica=verificaVitoria();
+        if(verifica!=""){
+            alert(verifica+"WINNER");
+            jogando=false;
+        }
+        atualizaTabuleiro();
+        quemJoga=0;
+    }
+}
+
+function verificaVitoria() {
+    var l, c;
+    //LINHAS
+    for(l=0;l<3;l++) {
+        if((jogo[l][0]==jogo[l][1])&&(jogo[l][1]==jogo[l][2])){
+            return jogo[l][0];
+            }
+        }
+    //COLUNAS
+        for(c=0;c<3;c++) {
+            if((jogo[0][c]==jogo[1][c])&&(jogo[1][c]==jogo[2][c])){
+                return jogo[0][c];   
+            }        
+        } 
+    //DIAGONAIS 
+        if((jogo[0][0]==jogo[1][1])&&(jogo[1][1]==jogo[2][0])){
+            return jogo[0][0]; 
+        }  
+        if((jogo[0][2]==jogo[1][1])&&(jogo[1][1]==jogo[2][2])){
+            return jogo[0][2]; 
+        }
+        return "";
+    }
+
+
 function jogar(p) {
     if((jogando)&&(quemJoga==0)){
         switch(p){
@@ -66,6 +113,11 @@ function jogar(p) {
             break;
         }
         atualizaTabuleiro();
+        verifica=verificaVitoria();
+        if(verifica!=""){
+            alert(verifica+"WINNER");
+            jogando=false;
+        }
     }
 }
 
