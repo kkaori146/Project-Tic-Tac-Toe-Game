28306b03916cadbc22ef05c597b0f6d820d8a010
Kaminishikawahara KK <kkaori146@gmail.com>
2021-11-02 12:31:25 +0900

    Switch and Position

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,55 @@ function play(p) {
                     quemJoga=1;
                 }
             break;
+            case 2:
+                if(jogo[0][1]=="") {
+                    jogo[0][1]="X";
+                    quemJoga=1;
+                }
+            break;
+            case 3:
+                if(jogo[0][2]=="") {
+                    jogo[0][2]="X";
+                    quemJoga=1;
+                }
+            break;
+            case 4:
+                if(jogo[1][0]=="") {
+                    jogo[1][0]="X";
+                    quemJoga=1;
+                }
+            break;
+            case 5:
+                if(jogo[1][1]=="") {
+                    jogo[1][1]="X";
+                    quemJoga=1;
+                }
+            break;
+            case 6:
+                if(jogo[1][2]=="") {
+                    jogo[1][2]="X";
+                    quemJoga=1;
+                }
+            break;
+            case 7:
+                if(jogo[2][0]=="") {
+                    jogo[2][0]="X";
+                    quemJoga=1;
+                }
+            break;
+            case 8:
+                if(jogo[2][1]=="") {
+                    jogo[2][1]="X";
+                    quemJoga=1;
+                }
+            break;
+            default:
+                if(jogo[2][2]=="") {
+                    jogo[2][2]="X";
+                    quemJoga=1;
+                }
+            break;
+
         }
     }
 }
